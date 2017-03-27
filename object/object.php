6182eec6e2b569ee5d48bc2ad1cbc7bf3fa2051d
grmnga <anna_titomer@mail.ru>
2017-03-27 14:16:57 +0500

    Выпадающий список с видами ремонта

--- object/object.php
+++ object/object.php
@@ -7,23 +7,29 @@ require_once("../database.php");
     if (!$link)
         echo "Error connect to data base";
 
-
+//ЗАПРОС ДАННЫХ ОБ ОБЪЕКТЕ
 $sql = "select *, (kv_1 + kv_2 + kv_3 + kv_4) as kv_sum  from objects where id = " . $_POST['id'];
-
 $result = mysqli_query($link, $sql);
+if (!$result)
+    die(mysqli_error($link));
+$data = mysqli_fetch_assoc($result);
 
-    if (!$result)
-        die(mysqli_error($link));
-    
-/*    $n = mysqli_num_rows($result);
-    $data = array();
-    
-    for ($i = 0; $i < $n; $i++)
-    {
-        $row = mysqli_fetch_assoc($result);
-        data[] = $row;
-    }*/
-    $data = mysqli_fetch_assoc($result);
-    //var_dump($data);
-    include("object.html");
+//var_dump($data);
+
+//СПРАВОЧНИК РЕМОНТНЫХ РАБОТ
+$sql = "select id, name from repairs_type";
+$result = mysqli_query($link, $sql);
+if (!$result)
+    die(mysqli_error($link));
+$n = mysqli_num_rows($result);
+$repairs_type = array();
+for ($i = 0; $i < $n; $i++)
+{
+    $row = mysqli_fetch_assoc($result);
+    $repairs_type[] = $row;
+}
+
+//var_dump($repairs_type);
+
+include("object.html");
 ?>
