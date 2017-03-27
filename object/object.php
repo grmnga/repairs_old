b52744714268d49e8d18e768aec20a04366abbb7
grmnga <anna_titomer@mail.ru>
2017-03-27 15:23:51 +0500

    выпадающие списки

--- object/object.php
+++ object/object.php
@@ -17,7 +17,7 @@ $data = mysqli_fetch_assoc($result);
 //var_dump($data);
 
 //СПРАВОЧНИК РЕМОНТНЫХ РАБОТ
-$sql = "select id, name from repairs_type";
+$sql = "select id, name from repairs_type ORDER BY name";
 $result = mysqli_query($link, $sql);
 if (!$result)
     die(mysqli_error($link));
@@ -43,7 +43,7 @@ for ($i = 0; $i < $n; $i++)
 }
 
 //СПРАВОЧНИК ИСПОЛНИТЕЛЕЙ
-$sql = "select id, name from executor";
+$sql = "select id, name from executor ORDER BY name";
 $result = mysqli_query($link, $sql);
 if (!$result)
     die(mysqli_error($link));
@@ -53,6 +53,98 @@ for ($i = 0; $i < $n; $i++)
 {
     $row = mysqli_fetch_assoc($result);
     $executor[] = $row;
+}
+
+
+//СПРАВОЧНИК КУРАТОРОВ
+$sql = "select id, name from curator ORDER BY name";
+$result = mysqli_query($link, $sql);
+if (!$result)
+    die(mysqli_error($link));
+$n = mysqli_num_rows($result);
+$curator = array();
+for ($i = 0; $i < $n; $i++)
+{
+    $row = mysqli_fetch_assoc($result);
+    $curator[] = $row;
+}
+
+//СПРАВОЧНИК ВИДОВ КОНСТРУКТИВА
+$sql = "select id, name from constructive_type ORDER BY name";
+$result = mysqli_query($link, $sql);
+if (!$result)
+    die(mysqli_error($link));
+$n = mysqli_num_rows($result);
+$constructive_type = array();
+for ($i = 0; $i < $n; $i++)
+{
+    $row = mysqli_fetch_assoc($result);
+    $constructive_type[] = $row;
+}
+
+//СПРАВОЧНИК ВИДОВ РАБОТ
+$sql = "select id, name from work_type ORDER BY name";
+$result = mysqli_query($link, $sql);
+if (!$result)
+    die(mysqli_error($link));
+$n = mysqli_num_rows($result);
+$work_type = array();
+for ($i = 0; $i < $n; $i++)
+{
+    $row = mysqli_fetch_assoc($result);
+    $work_type[] = $row;
+}
+
+//СПРАВОЧНИК МАТЕРИАЛОВ
+$sql = "select id, name from material ORDER BY name";
+$result = mysqli_query($link, $sql);
+if (!$result)
+    die(mysqli_error($link));
+$n = mysqli_num_rows($result);
+$material = array();
+for ($i = 0; $i < $n; $i++)
+{
+    $row = mysqli_fetch_assoc($result);
+    $material[] = $row;
+}
+
+//СПРАВОЧНИК ЗАКАЗЧИКОВ
+$sql = "select id, name from customer ORDER BY name";
+$result = mysqli_query($link, $sql);
+if (!$result)
+    die(mysqli_error($link));
+$n = mysqli_num_rows($result);
+$customer = array();
+for ($i = 0; $i < $n; $i++)
+{
+    $row = mysqli_fetch_assoc($result);
+    $customer[] = $row;
+}
+
+//СПРАВОЧНИК ПОДРЯДЧИКОВ
+$sql = "select id, name from contractor ORDER BY name";
+$result = mysqli_query($link, $sql);
+if (!$result)
+    die(mysqli_error($link));
+$n = mysqli_num_rows($result);
+$contractor = array();
+for ($i = 0; $i < $n; $i++)
+{
+    $row = mysqli_fetch_assoc($result);
+    $contractor[] = $row;
+}
+
+//СПРАВОЧНИК ЕДИНИЦ ИЗМЕРЕНИЯ
+$sql = "select id, name from unit ORDER BY name";
+$result = mysqli_query($link, $sql);
+if (!$result)
+    die(mysqli_error($link));
+$n = mysqli_num_rows($result);
+$unit = array();
+for ($i = 0; $i < $n; $i++)
+{
+    $row = mysqli_fetch_assoc($result);
+    $unit[] = $row;
 }
 
 //var_dump($constructive);
